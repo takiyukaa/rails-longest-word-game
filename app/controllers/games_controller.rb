5f06d6ba3d7be841648432da65b267c5fd9456cc
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-18 01:10:32 +0900

    add api to check a word if it is valid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @alphabet = ('a'..'z').to_a
@@ -6,12 +8,16 @@ class GamesController < ApplicationController
 
   def score
     @letters = params[:letters]
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    @answer = JSON.parse(open(url).read)
 
     params[:word].split('').each do |letter|
-      if @letters.include?(letter)
-        @result = "Congraturations! #{params[:word]} is ok"
+      if @letters.include?(letter) && @answer["found"] == true
+        @result = "Congraturations! #{params[:word]} is valid word:)"
+      elsif @letters.include?(letter) && @answer["found"] == false
+        @result = "sorry, #{params[:word]} does not seem to be a English word..."
       else
-        @result = "sorry, #{params[:word]} can't be built"
+        @result = "sorry, but #{params[:word]} can't be built out of [#{@letters}]"
       end
     end
   end
